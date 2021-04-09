cd6685962ea2db326f46930ab326b06fd5d6495a
Anna Strik <anna.strik1@gmail.com>
2021-04-09 11:39:31 -0700

    firefox added

--- wdio.conf.js
+++ wdio.conf.js
@@ -53,24 +53,24 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://docs.saucelabs.com/reference/platforms-configurator
   //
-  capabilities: [{
-
+  capabilities: [
+    {
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 firefox instances available you can make sure that not more than
     // 5 instances get started at a time.
-    maxInstances: 2,
-    //
-    browserName: 'chrome',
-    acceptInsecureCerts: true
+      maxInstances: 2,
+      //
+      browserName: 'chrome',
+      acceptInsecureCerts: true
     // If outputDir is provided WebdriverIO can capture driver session logs
     // it is possible to configure which logTypes to include/exclude.
     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
     // excludeDriverLogs: ['bugreport', 'server'],
-  },
-  {
-    maxInstances: 1,
-    browserName: 'firefox',
-  },
+    },
+    {
+      maxInstances: 2,
+      browserName: 'firefox',
+    },
   ],
   //
   // ===================
